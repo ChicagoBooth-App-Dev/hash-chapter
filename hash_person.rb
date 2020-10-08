5aff66eda8cbab8c55e292f1334614a18ade9a5c
KevinDelaFuente <kevin.delafuente@chicagobooth.edu>
2020-10-08 05:46:12 +0000

    hash_person test passed

--- hash_person.rb
+++ hash_person.rb
@@ -1,2 +1,11 @@
 # Ask for a name, age, and occupation; store the values in a Hash; display the Hash.
 p "Enter a name, age, and occupation separated by spaces:"
+
+hash = gets.chomp.split
+person = Hash.new
+
+person.store(:name, hash[0])
+person.store(:age, hash[1].to_i)
+person.store(:occupation, hash[2])
+
+puts person
